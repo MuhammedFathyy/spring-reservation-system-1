87ca9e0c6e7d0259020ea07bf4d711359880a56f
bassantyasser <bassantyasser302@gmail.com>
2023-02-21 15:58:23 +0200

    first commit

---
+++ src/main/java/com/gradproject/yourspace/YourspaceApplication.java
@@ -0,0 +1,13 @@
+package com.gradproject.yourspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourspaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YourspaceApplication.class, args);
+	}
+
+}
