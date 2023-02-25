a07c70c4d55500ab9514aa71df1d382f7504d6e1
Muhammed Fathy <mfathy56734@gmail.com>
2023-02-25 12:11:46 +0200

    final update for entity class user

--- src/main/java/com/gradproject/yourspace/entity/User.java
+++ src/main/java/com/gradproject/yourspace/entity/User.java
@@ -2,52 +2,53 @@ package com.gradproject.yourspace.entity;
 
 import com.sun.istack.NotNull;
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @Table(name = "user")
 public class User {
-        @Id
-        @GeneratedValue(strategy =  GenerationType.IDENTITY)
-        @Column(name = "UserID")
-        private int UserId;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "UserID")
+    private int UserId;
 
-        @Column(name = "email")
-        @NotNull
-        private String email;
+    @Column(name = "email")
+    @NotNull
+    private String email;
 
-        @Column(name = "first_name")
-        private String firstName;
+    @Column(name = "first_name")
+    private String firstName;
 
-        @Column(name = "last_name")
-        private String lastName;
+    @Column(name = "last_name")
+    private String lastName;
 
-        @Column(name = "password")
-        private String password;
+    @Column(name = "password")
+    private String password;
 
-        @Column(name = "mobile_no")
-        private String phoneNo;
+    @Column(name = "mobile_no")
+    private String phoneNo;
 
-        @Column(name = "address")
-        private String address;
+    @Column(name = "address")
+    private String address;
 
-        @Column(name = "birthdate")
-        private String birthDate;
+    @Column(name = "birthdate")
+    private String birthDate;
 
-        @Column(name = "bio")
-        private String bio;
+    @Column(name = "bio")
+    private String bio;
 
-        @Column(name = "points")
-        private int points;
+    @Column(name = "points")
+    private int points;
 
-        @Column(name = "profile_picture")
-        private String picture;
+    @Column(name = "profile_picture")
+    private String picture;
 
-        @OneToMany(mappedBy = "user", cascade = {CascadeType.ALL})
-        private List<Request> requests;
+    @OneToMany(mappedBy = "user", cascade = {CascadeType.ALL})
+    private List<Request> requests;
 
-        @OneToMany(mappedBy = "user", cascade = {CascadeType.ALL})
-        private List<Booking> bookings;
+    @OneToMany(mappedBy = "user", cascade = {CascadeType.ALL})
+    private List<Booking> bookings;
 
 
     public User() {
@@ -56,140 +57,153 @@ public class User {
     public User(String email, String firstName, String lastName, String password,
                 String phoneNo, String address, String birthDate, String bio,
                 int points, String picture, List<Request> requests, List<Booking> bookings) {
-            this.email = email;
-            this.firstName = firstName;
-            this.lastName = lastName;
-            this.password = password;
-            this.phoneNo = phoneNo;
-            this.address = address;
-            this.birthDate = birthDate;
-            this.bio = bio;
-            this.points = points;
-            this.picture = picture;
-            this.requests = requests;
-            this.bookings = bookings;
-        }
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.password = password;
+        this.phoneNo = phoneNo;
+        this.address = address;
+        this.birthDate = birthDate;
+        this.bio = bio;
+        this.points = points;
+        this.picture = picture;
+        this.requests = requests;
+        this.bookings = bookings;
+    }
 
     public int getUserId() {
-            return UserId;
-        }
+        return UserId;
+    }
 
-        public void setUserId(int userId) {
-            UserId = userId;
-        }
+    public void setUserId(int userId) {
+        UserId = userId;
+    }
 
-        public String getEmail() {
-            return email;
-        }
+    public String getEmail() {
+        return email;
+    }
 
-        public void setEmail(String email) {
-            this.email = email;
-        }
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-        public String getFirstName() {
-            return firstName;
-        }
+    public String getFirstName() {
+        return firstName;
+    }
 
-        public void setFirstName(String firstName) {
-            this.firstName = firstName;
-        }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 
-        public String getLastName() {
-            return lastName;
-        }
+    public String getLastName() {
+        return lastName;
+    }
 
-        public void setLastName(String lastName) {
-            this.lastName = lastName;
-        }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
-        public String getPassword() {
-            return password;
-        }
+    public String getPassword() {
+        return password;
+    }
 
-        public void setPassword(String password) {
-            this.password = password;
-        }
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
-        public String getPhoneNo() {
-            return phoneNo;
-        }
+    public String getPhoneNo() {
+        return phoneNo;
+    }
 
-        public void setPhoneNo(String phoneNo) {
-            this.phoneNo = phoneNo;
-        }
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
 
-        public String getAddress() {
-            return address;
-        }
+    public String getAddress() {
+        return address;
+    }
 
-        public void setAddress(String address) {
-            this.address = address;
-        }
+    public void setAddress(String address) {
+        this.address = address;
+    }
 
-        public String getBirthDate() {
-            return birthDate;
-        }
+    public String getBirthDate() {
+        return birthDate;
+    }
 
-        public void setBirthDate(String birthDate) {
-            this.birthDate = birthDate;
-        }
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
 
-        public String getBio() {
-            return bio;
-        }
+    public String getBio() {
+        return bio;
+    }
 
-        public void setBio(String bio) {
-            this.bio = bio;
-        }
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
 
-        public int getPoints() {
-            return points;
-        }
+    public int getPoints() {
+        return points;
+    }
 
-        public void setPoints(int points) {
-            this.points = points;
-        }
+    public void setPoints(int points) {
+        this.points = points;
+    }
 
-        public String getPicture() {
-            return picture;
-        }
+    public String getPicture() {
+        return picture;
+    }
 
-        public void setPicture(String picture) {
-            this.picture = picture;
-        }
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
 
-        public List<Request> getRequests() {
-            return requests;
-        }
+    public List<Request> getRequests() {
+        return requests;
+    }
 
-        public void setRequests(List<Request> requests) {
-            this.requests = requests;
-        }
+    public void setRequests(List<Request> requests) {
+        this.requests = requests;
+    }
 
-        public List<Booking> getBookings() {
-            return bookings;
-        }
+    public List<Booking> getBookings() {
+        return bookings;
+    }
 
-        public void setBookings(List<Booking> bookings) {
-            this.bookings = bookings;
-        }
+    public void setBookings(List<Booking> bookings) {
+        this.bookings = bookings;
+    }
 
-        @Override
-        public String toString() {
-            return "User{" +
-                    "UserId=" + UserId +
-                    ", email='" + email + '\'' +
-                    ", firstName='" + firstName + '\'' +
-                    ", lastName='" + lastName + '\'' +
-                    ", password='" + password + '\'' +
-                    ", phoneNo='" + phoneNo + '\'' +
-                    ", address='" + address + '\'' +
-                    ", birthDate='" + birthDate + '\'' +
-                    ", bio='" + bio + '\'' +
-                    ", points=" + points +
-                    ", picture='" + picture + '\'' +
-                    ", requests=" + requests +
-                    ", bookings=" + bookings +
-                    '}';
-        }
+    @Override
+    public String toString() {
+        return "User{" +
+                "UserId=" + UserId +
+                ", email='" + email + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", password='" + password + '\'' +
+                ", phoneNo='" + phoneNo + '\'' +
+                ", address='" + address + '\'' +
+                ", birthDate='" + birthDate + '\'' +
+                ", bio='" + bio + '\'' +
+                ", points=" + points +
+                ", picture='" + picture + '\'' +
+                ", requests=" + requests +
+                ", bookings=" + bookings +
+                '}';
+    }
+
+    public void addRequest(Request request) {
+        if (this.requests == null) this.requests = new ArrayList<>();
+        requests.add(request);
+        request.setUser(this);
+    }
+
+    public void addBooking(Booking booking) {
+        if (bookings == null) bookings = new ArrayList<>();
+        bookings.add(booking);
+        booking.setUser(this);
+    }
+
 }
