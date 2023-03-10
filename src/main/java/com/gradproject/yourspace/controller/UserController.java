df29c28a089764d57befdb8786f8bb64116a9a03
Muhammed Fathy <mfathy56734@gmail.com>
2023-03-10 21:45:34 +0200

    Patch request is modified to update different data types such as Integers and Dates

--- src/main/java/com/gradproject/yourspace/controller/UserController.java
+++ src/main/java/com/gradproject/yourspace/controller/UserController.java
@@ -4,9 +4,16 @@ package com.gradproject.yourspace.controller;
 import com.gradproject.yourspace.entity.User;
 import com.gradproject.yourspace.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.*;
 
+import java.lang.reflect.Field;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.text.ParseException;
 
 @RestController
 @RequestMapping("api/user")
@@ -36,10 +43,36 @@ public class UserController {
         userService.saveUser(user);
     }
 
-    @PutMapping()
-    public void updateUser(@RequestBody User user)
+    @PatchMapping("{userId}")
+    public void updateUser(@PathVariable int userId, @RequestBody HashMap<String , Object> fields)
     {
+        User user = userService.getUser(userId);
+        //you can check for this user if it is existing or not
+        fields.forEach((key,value)->{
+            Field field = ReflectionUtils.findField(User.class, key);
+            field.setAccessible(true);
+            DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+            boolean normalParameter = true;
+
+            try {
+                int points = Integer.parseInt(value.toString());
+                ReflectionUtils.setField(field , user , points);
+                normalParameter = false;
+
+            }catch (NumberFormatException exc){
+                try{
+                    Date date = dateFormat.parse((String)value);
+                    ReflectionUtils.setField(field , user , date);
+                    normalParameter = false;
+                }catch ( ParseException e) {}
+
+            if(normalParameter){
+                ReflectionUtils.setField(field , user , value);
+            }
+            }
+        });
         userService.saveUser(user);
+
     }
 
     @DeleteMapping("{userId}")
