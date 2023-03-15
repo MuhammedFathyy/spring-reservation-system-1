bd0f906092c5600e187b7cc490173a39df8008bd
bassantyasser <bassantyasser302@gmail.com>
2023-03-15 22:02:15 +0200

    modifing space controller and space dao

--- src/main/java/com/gradproject/yourspace/dao/SpaceDAO.java
+++ src/main/java/com/gradproject/yourspace/dao/SpaceDAO.java
@@ -5,10 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SpaceRepository extends JpaRepository<Space,Integer> {
-
-Space findSpaceBySpaceId(int id);
-
+public interface SpaceDAO extends JpaRepository<Space,Integer> {
+    Space findSpaceBySpaceId(int id);
 
 
 }
