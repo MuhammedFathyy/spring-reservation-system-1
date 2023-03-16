95d87077b26a19c355cf99228782854aa92d5cde
MuhammadMoataz <72518785+MuhammadMoataz@users.noreply.github.com>
2023-03-16 10:48:03 +0200

    Shifting request to JPA

--- src/main/java/com/gradproject/yourspace/dao/RequestDAO.java
+++ src/main/java/com/gradproject/yourspace/dao/RequestDAO.java
@@ -1,15 +1,8 @@
 package com.gradproject.yourspace.dao;
 
 import com.gradproject.yourspace.entity.Request;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 
-public interface RequestDAO {
-    List<Request> findAll();
-
-    Request findById(int requestId);
-
-    void saveRequest(Request request);
-
-    void deleteRequest(int requestId);
+public interface RequestDAO extends JpaRepository<Request, Integer> {
 }
