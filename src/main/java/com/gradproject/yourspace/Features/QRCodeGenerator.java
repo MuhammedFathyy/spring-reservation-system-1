7265aa8626552dbf93b950d7dd8c753cd140160e
bassantyasser <bassantyasser302@gmail.com>
2023-04-16 22:20:44 +0200

    QR code to service

--- src/main/java/com/gradproject/yourspace/Features/QRCodeGenerator.java
+++ src/main/java/com/gradproject/yourspace/Features/QRCodeGenerator.java
@@ -7,7 +7,7 @@ import com.google.zxing.client.j2se.MatrixToImageConfig;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
-import com.gradproject.yourspace.dto.BookingDTO;
+import com.gradproject.yourspace.entity.Booking;
 import com.gradproject.yourspace.service.BookingService;
 import io.jsonwebtoken.JwtBuilder;
 import io.jsonwebtoken.SignatureAlgorithm;
@@ -22,9 +22,6 @@ import org.springframework.stereotype.Component;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.nio.file.FileSystems;
-import java.nio.file.Path;
-import java.util.List;
 
 
 @Component
@@ -38,16 +35,16 @@ public class QRCodeGenerator {
     }
 
 
-    //create the image for QR Code and save it in the specified path
-    public static void generateQRCodeImage(String text, int width, int height, String filePath)
-            throws WriterException, IOException {
-        QRCodeWriter qrCodeWriter = new QRCodeWriter();
-        BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, width, height);
-
-        Path path = FileSystems.getDefault().getPath(filePath);
-        MatrixToImageWriter.writeToPath(bitMatrix, "PNG", path);
-
-    }
+//    //create the image for QR Code and save it in the specified path
+//    public static void generateQRCodeImage(String text, int width, int height, String filePath)
+//            throws WriterException, IOException {
+//        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+//        BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, width, height);
+//
+//        Path path = FileSystems.getDefault().getPath(filePath);
+//        MatrixToImageWriter.writeToPath(bitMatrix, "PNG", path);
+//
+//    }
 
     //create the image in byte
     public static byte[] getQRCodeImage(String text, int width, int height) throws WriterException, IOException {
@@ -62,19 +59,18 @@ public class QRCodeGenerator {
         return pngData;
     }
 
-    public static String getToken() throws IOException {
+    public static String getToken(Booking booking) throws IOException {
 
         //for testing purpose
         ObjectMapper mapper = new ObjectMapper();
-        List<BookingDTO> booking =bookingService.getBookingsHistory(2);
-        BookingDTO book=booking.get(0);
-
-        String jsonBooking = mapper.writeValueAsString(book);
+        String jsonBooking = mapper.writeValueAsString(booking);
         JwtBuilder builder = new DefaultJwtBuilder();
         builder.claim("booking" , jsonBooking );
         builder.signWith(SignatureAlgorithm.HS256,(new HmacKey("secretkeysecretkeysecretkeysecretkeysecretkeysecretkey".getBytes())));
         return builder.compact();
     }
+
+    //will be used by the front end to encode the token
     public static String consumeToken(String token) throws InvalidJwtException {
         JwtConsumer consumer = new JwtConsumerBuilder().setVerificationKey(new HmacKey("secretkeysecretkeysecretkeysecretkeysecretkeysecretkey".getBytes())).build();
         return consumer.processToClaims(token).toJson();
--- src/main/java/com/gradproject/yourspace/Features/QRCodeHandler.java
+++ src/main/java/com/gradproject/yourspace/Features/QRCodeHandler.java
@@ -2,53 +2,40 @@ package com.gradproject.yourspace.Features;
 
 
 import com.google.zxing.WriterException;
+import com.gradproject.yourspace.entity.Booking;
 import org.jose4j.jwt.consumer.InvalidJwtException;
-import org.springframework.ui.Model;
+import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.util.Base64;
 
 
+
+@Service
 public class QRCodeHandler {
 
-    private static final String QR_CODE_IMAGE_PATH = "./src/main/resources/templates/QRCode.png";
-
-    public String getQRCode(Model model){
-
-
-         String url;
+    public String getQRCode(Booking booking){
+        String url;
         byte[] image = new byte[0];
         try {
 
-            String token= QRCodeGenerator.getToken();
-
+            String token= QRCodeGenerator.getToken(booking);
             String Consumed= QRCodeGenerator.consumeToken(token);
-
             //should be extracted by the front end and consume the token with consume function
-             url= "www.yourspace.bookings/"+token;
+             url= "http://localhost:8080/api/bookings"+token;
             // Generate and Return Qr Code in Byte Array
             image = QRCodeGenerator.getQRCodeImage(url,1000,1000);
 
-            // Generate and Save Qr Code Image in static/image folder
-            QRCodeGenerator.generateQRCodeImage(url,1000,1000,QR_CODE_IMAGE_PATH);
-            model.addAttribute("url",url);
-            System.out.println(url);
 
         } catch (WriterException | IOException e) {
             e.printStackTrace();
         } catch (InvalidJwtException ex) {
             throw new RuntimeException(ex);
         }
-
         // Convert Byte Array into Base64 Encode String
         String qrcode = Base64.getEncoder().encodeToString(image);
 
-
-        model.addAttribute("qrcode",qrcode);
-
-        System.out.println(model.getAttribute("qrcode"));
-
-        return "qrcode";
+        return qrcode;
 
     }
 }
