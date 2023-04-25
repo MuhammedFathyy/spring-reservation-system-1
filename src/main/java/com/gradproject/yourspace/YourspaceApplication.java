e9defbe9a49e65b8eae8ee5df8e23048b06a26d3
MuhammadMoataz <72518785+MuhammadMoataz@users.noreply.github.com>
2023-04-25 23:22:25 +0200

    Adding ModelMapper Bean

--- src/main/java/com/gradproject/yourspace/YourspaceApplication.java
+++ src/main/java/com/gradproject/yourspace/YourspaceApplication.java
@@ -1,13 +1,20 @@
 package com.gradproject.yourspace;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 import javax.annotation.PostConstruct;
 import java.util.TimeZone;
 
 @SpringBootApplication
 public class YourspaceApplication {
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
 
 	public static void main(String[] args) {
 		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
