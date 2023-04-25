420f85ce0c0c66cdef4c7c91fa2f1a7f92c3c011
MuhammadMoataz <72518785+MuhammadMoataz@users.noreply.github.com>
2023-04-26 00:25:20 +0200

    Fixed BookingDTO

--- src/main/java/com/gradproject/yourspace/dto/BookingDTO.java
+++ src/main/java/com/gradproject/yourspace/dto/BookingDTO.java
@@ -1,5 +1,7 @@
 package com.gradproject.yourspace.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import javax.validation.constraints.NotNull;
 import java.sql.Time;
 import java.time.LocalDate;
@@ -7,13 +9,64 @@ import java.time.LocalDate;
 public class BookingDTO {
     private int id;
     @NotNull
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm:ss")
     private Time startTime;
     @NotNull
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm:ss")
     private Time endTime;
     @NotNull
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
     private LocalDate date;
     @NotNull
     private Integer roomId;
     @NotNull
     private Integer userId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Time getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Time startTime) {
+        this.startTime = startTime;
+    }
+
+    public Time getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Time endTime) {
+        this.endTime = endTime;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public Integer getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(Integer roomId) {
+        this.roomId = roomId;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
 }
