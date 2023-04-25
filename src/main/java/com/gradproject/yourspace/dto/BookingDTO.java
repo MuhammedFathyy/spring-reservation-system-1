2347ff16402b64e32770981998d03513a0776c51
MuhammadMoataz <72518785+MuhammadMoataz@users.noreply.github.com>
2023-04-25 23:21:52 +0200

    Updating BookingDTO

--- src/main/java/com/gradproject/yourspace/dto/BookingDTO.java
+++ src/main/java/com/gradproject/yourspace/dto/BookingDTO.java
@@ -1,70 +1,19 @@
 package com.gradproject.yourspace.dto;
 
+import javax.validation.constraints.NotNull;
 import java.sql.Time;
 import java.time.LocalDate;
 
 public class BookingDTO {
     private int id;
+    @NotNull
     private Time startTime;
+    @NotNull
     private Time endTime;
+    @NotNull
     private LocalDate date;
+    @NotNull
     private Integer roomId;
+    @NotNull
     private Integer userId;
-
-    public BookingDTO(int id, Time startTime, Time endTime, LocalDate date, Integer roomId, Integer userId) {
-        this.id = id;
-        this.startTime = startTime;
-        this.endTime = endTime;
-        this.date = date;
-        this.roomId = roomId;
-        this.userId = userId;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public Time getStartTime() {
-        return startTime;
-    }
-
-    public void setStartTime(Time startTime) {
-        this.startTime = startTime;
-    }
-
-    public Time getEndTime() {
-        return endTime;
-    }
-
-    public void setEndTime(Time endTime) {
-        this.endTime = endTime;
-    }
-
-    public LocalDate getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDate date) {
-        this.date = date;
-    }
-
-    public Integer getRoomId() {
-        return roomId;
-    }
-
-    public void setRoomId(Integer roomId) {
-        this.roomId = roomId;
-    }
-
-    public Integer getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Integer userId) {
-        this.userId = userId;
-    }
 }
