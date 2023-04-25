6a1a211b7cf68f6e800efdaadbf59cc410598541
MuhammadMoataz <72518785+MuhammadMoataz@users.noreply.github.com>
2023-04-25 23:23:54 +0200

    Adding ModelMapper to BookingController

--- src/main/java/com/gradproject/yourspace/controller/BookingController.java
+++ src/main/java/com/gradproject/yourspace/controller/BookingController.java
@@ -3,37 +3,50 @@ package com.gradproject.yourspace.controller;
 import com.gradproject.yourspace.dto.BookingDTO;
 import com.gradproject.yourspace.entity.Booking;
 import com.gradproject.yourspace.service.BookingService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.HashMap;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/bookings")
 public class BookingController {
-    @Autowired
     private BookingService bookingService;
+    private ModelMapper modelMapper;
+
+    public BookingController(BookingService bookingService, ModelMapper modelMapper) {
+        this.bookingService = bookingService;
+        this.modelMapper = modelMapper;
+    }
 
     @GetMapping()
-    public List<Booking> findAll() {
-        return bookingService.findAll();
+    public List<BookingDTO> findAll() {
+        return bookingService.findAll()
+                .stream()
+                .map(booking -> modelMapper.map(booking, BookingDTO.class))
+                .collect(Collectors.toList());
     }
 
     @GetMapping("/{bookId}")
-    public Booking findById(@PathVariable int bookId) {
-        return bookingService.findById(bookId);
+    public BookingDTO findById(@PathVariable int bookId) {
+        Booking booking = bookingService.findById(bookId);
+        return modelMapper.map(booking, BookingDTO.class);
     }
 
     @PostMapping()
-    public ResponseEntity<String> saveBooking(@RequestBody @Valid Booking booking) {
+    public ResponseEntity<String> saveBooking(@RequestBody @Valid BookingDTO bookingDTO) {
+        Booking booking = modelMapper.map(bookingDTO, Booking.class);
         return bookingService.saveBooking(booking);
     }
 
+
     @PutMapping()
-    public ResponseEntity<String> updateBooking(@RequestBody @Valid Booking booking) {
+    public ResponseEntity<String> updateBooking(@RequestBody @Valid BookingDTO bookingDTO) {
+        Booking booking = modelMapper.map(bookingDTO, Booking.class);
         return bookingService.updateBooking(booking);
     }
 
@@ -48,6 +61,7 @@ public class BookingController {
     }
 
     @GetMapping("bookingsHistory/{userId}")
+    // TODO: mod and push
     public List<BookingDTO> getBookingsHistory(@PathVariable int userId) {
         return bookingService.getBookingsHistory(userId);
     }
